5c6a130e025481c6db8c3802a7627a9a75578923
Rafael <rcarneiro782@gmail.com>
2022-04-06 18:57:44 -0300

    controle

---
+++ etapas.js
@@ -0,0 +1,53 @@
+let etapas = [
+    {
+        título: 'vereador',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome:'Daniel',
+                partido:'verde',
+                fotos:[
+                    {URL:'38111.jpg', legenda:'vereador'}
+                ]
+            },
+
+            {
+                numero: 13136,
+                nome:'Humberto',
+                partido:'PT',
+                fotos:[
+                    {URL:'13136.jpg', legenda:'vereador'}
+                ]
+            },
+
+        ]
+    },
+    {
+        titulo:'Prefeito',
+        numeros:2,
+        candidatos:[
+        {
+            numero:70,
+            nome:'Jarbas',
+            partido:'PSDB',
+            vice:'Mendonça',
+            fotos:[
+                {URL:'70.jpg', legenda:'Prefeito'},
+                {URL:'70.jpg', legenda:'vice-prefeito', small:false}
+            ]
+        
+    },
+    {
+        numero:13,
+        nome:'Marília Arraes',
+        partido:'pt',
+        vice:'João Paulo',
+        fotos:[
+            {URL:'13.jpg', legenda:'Prefeito'},
+            {URL:'13.jpg', legenda:'vice-prefeito', small:false}
+        ]
+    }
+    ]
+}
+];
